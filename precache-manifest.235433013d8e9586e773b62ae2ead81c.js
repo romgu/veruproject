a7c0de137a3eafe6aedd08c8048a6c1c339a58bf
romgu <gmarca20@gmail.com>
2019-06-03 14:39:11 -0300

    Updates

---
+++ precache-manifest.235433013d8e9586e773b62ae2ead81c.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13de37b237a55bb16e50c626efd66ef4",
+    "url": "/veruproject/index.html"
+  },
+  {
+    "revision": "dc3d4ebe4ca62e54810c",
+    "url": "/veruproject/static/css/2.9522b9fe.chunk.css"
+  },
+  {
+    "revision": "1e1f1dc3228c4274d2ec",
+    "url": "/veruproject/static/css/main.071cb504.chunk.css"
+  },
+  {
+    "revision": "dc3d4ebe4ca62e54810c",
+    "url": "/veruproject/static/js/2.a2b12e65.chunk.js"
+  },
+  {
+    "revision": "1e1f1dc3228c4274d2ec",
+    "url": "/veruproject/static/js/main.e34d7e80.chunk.js"
+  },
+  {
+    "revision": "c182fbeec103b83d228e",
+    "url": "/veruproject/static/js/runtime~main.d63868f3.js"
+  },
+  {
+    "revision": "5382cf91d09039732b40f0874116339e",
+    "url": "/veruproject/static/media/Design_01.5382cf91.jpg"
+  },
+  {
+    "revision": "4fa363527af5175de7d9157d3e441a2b",
+    "url": "/veruproject/static/media/Design_02.4fa36352.jpg"
+  },
+  {
+    "revision": "0f6e082f42f5247d82c3ade9c2a8f928",
+    "url": "/veruproject/static/media/Gilroy-ExtraBold.0f6e082f.otf"
+  },
+  {
+    "revision": "c62aded729bf7146d491275e5019d7fc",
+    "url": "/veruproject/static/media/Gilroy-Light.c62aded7.otf"
+  },
+  {
+    "revision": "cd91892e1b9852f45192b82a4ac0cc81",
+    "url": "/veruproject/static/media/Header_BG.cd91892e.jpg"
+  },
+  {
+    "revision": "a94736935cc3d7ce29bd535047e1a9c9",
+    "url": "/veruproject/static/media/Personas01.a9473693.jpg"
+  },
+  {
+    "revision": "f20c0f8e45452a6d81303406f6e7dd3c",
+    "url": "/veruproject/static/media/Personas02.f20c0f8e.jpg"
+  },
+  {
+    "revision": "04bee4759f9ed44b8c6301b5632c24ce",
+    "url": "/veruproject/static/media/Personas03.04bee475.jpg"
+  },
+  {
+    "revision": "52b1f53df59b3185e570802862c5e895",
+    "url": "/veruproject/static/media/Veru_Logo_01.52b1f53d.jpg"
+  },
+  {
+    "revision": "1a0f305ec99e9679430d1e3d90677442",
+    "url": "/veruproject/static/media/Veru_Perspective_Web.1a0f305e.jpg"
+  },
+  {
+    "revision": "7d1f732d1930ff2aaf4c82c41784b966",
+    "url": "/veruproject/static/media/Vision_01.7d1f732d.jpg"
+  },
+  {
+    "revision": "9fb33ae124e6c7a7650a1e1ea3be7f10",
+    "url": "/veruproject/static/media/Vision_02.9fb33ae1.jpg"
+  },
+  {
+    "revision": "02d9ef7c8d4c7b9399e55de399740dfa",
+    "url": "/veruproject/static/media/Vision_03.02d9ef7c.jpg"
+  },
+  {
+    "revision": "03c6faf3dfbf9478ef15e88e0f172bce",
+    "url": "/veruproject/static/media/Vision_04.03c6faf3.jpg"
+  },
+  {
+    "revision": "323a6d7310d87cf06771dcc26365ae72",
+    "url": "/veruproject/static/media/Vision_05.323a6d73.jpg"
+  },
+  {
+    "revision": "ed4d724d3e6d04b92cfd0ae233b35b98",
+    "url": "/veruproject/static/media/Vision_06.ed4d724d.jpg"
+  }
+]);
